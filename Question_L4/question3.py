1afaf29bd545cc705a4cd2e96ac2b9350b08aa44
LuisT_ls <luishg213@outlook.com>
2022-06-07 13:52:23 -0300

    commit

--- Question_L4/question3.py
+++ Question_L4/question3.py
@@ -3,10 +3,9 @@ par = []
 impar = []
 
 for i in range(num):
-    
     a: int = input().split()
     
-    if int(a) % 2 == 0:
+    if a % 2 == 0:
         par.append(a)
     else:
         impar.append(a)
